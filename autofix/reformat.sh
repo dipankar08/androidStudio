bbb7ae94f51cf31f9cab7b7f4c8fde3259ccd225
Dipankar Dutta <dip@Dipankar-Mac.local>
2018-02-22 00:44:54 +0000

    autofix

--- autofix/reformat.sh
+++ autofix/reformat.sh
@@ -1,1 +1,4 @@
 java -jar google-java-format-1.5-all-deps.jar -r ../CoolUI/app/src/main/java/in/co/dipankar/coolui/*/*
+java -jar google-java-format-1.5-all-deps.jar -r ../fmradioindia/app/src/main/java/in/peerreview/fmradioindia/*/*
+java -jar google-java-format-1.5-all-deps.jar -r ../fmradioindia/app/src/main/java/in/peerreview/fmradioindia/*/*/*
+java -jar google-java-format-1.5-all-deps.jar -r ../fmradioindia/app/src/main/java/in/peerreview/fmradioindia/*/*/*/*
