72a365a8ab079270bd358f0a218efda3daea5027
Dipankar Dutta <dip@Dipankar-Mac.local>
2018-02-22 21:04:37 +0000

    Lets have L.d and L.e()

---
+++ SampleLib/preferenceutils/src/main/java/in/co/dipankar/preferenceutils/L.java
@@ -0,0 +1,17 @@
+package in.co.dipankar.preferenceutils;
+
+import android.util.Log;
+
+/**
+ * Created by dip on 2/22/18.
+ */
+
+public class L{
+    private static final String TAG = "DIPANKAR";
+    public static void d(String msg){
+        Log.d(TAG, msg);
+    }
+    public static void e(String msg){
+        Log.e(TAG, msg);
+    }
+}
